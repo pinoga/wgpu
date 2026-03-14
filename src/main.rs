07a76ae828b65f75333c605c29a867ce9d7579ce
pinoga <fernando_bizzotto@hotmail.com>
2026-03-14 16:06:50 +0000

    feat: open window

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,58 @@
-use wgpu::{Instance, RequestAdapterOptions, SurfaceTarget, rwh::HasWindowHandle};
-use winit::{
-    application::ApplicationHandler, event::WindowEvent, event_loop::EventLoop, window::Window,
-};
+use std::sync::Arc;
+
+use wgpu::{Instance, RequestAdapterOptions};
 
 #[derive(Default)]
 struct App {
-    window: Option<Window>,
+    graphics_context: Option<GraphicsContext>,
 }
 
-impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        self.window = Some(
-            event_loop
-                .create_window(Window::default_attributes())
+struct GraphicsContext {
+    window: Arc<winit::window::Window>,
+    wgpu_instance: wgpu::Instance,
+    wgpu_surface: Option<wgpu::Surface<'static>>,
+    wgpu_adapter: Option<wgpu::Adapter>,
+}
+
+impl GraphicsContext {
+    fn initialize_surface(&mut self) {
+        self.wgpu_surface = Some(
+            self.wgpu_instance
+                .create_surface(self.window.clone())
                 .unwrap(),
         );
+    }
+
+    fn initialize_adapter(&mut self) {
+        self.wgpu_adapter = Some(
+            pollster::block_on(self.wgpu_instance.request_adapter(&RequestAdapterOptions {
+                compatible_surface: self.wgpu_surface.as_ref(),
+                ..Default::default()
+            }))
+            .unwrap(),
+        );
+    }
+}
+
+impl winit::application::ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        if !self.graphics_context.is_some() {
+            self.graphics_context = Some(GraphicsContext {
+                wgpu_instance: Instance::default(),
+                window: Arc::new(
+                    event_loop
+                        .create_window(winit::window::Window::default_attributes())
+                        .unwrap(),
+                ),
+                wgpu_adapter: None,
+                wgpu_surface: None,
+            })
+        }
+
+        let ctx = self.graphics_context.as_mut().unwrap();
+
+        ctx.initialize_surface();
+        ctx.initialize_adapter();
     }
 
     fn window_event(
@@ -24,9 +62,15 @@ impl ApplicationHandler for App {
         event: winit::event::WindowEvent,
     ) {
         match event {
-            WindowEvent::CloseRequested => event_loop.exit(),
-            WindowEvent::RedrawRequested => {
-                self.window.as_ref().unwrap().request_redraw();
+            winit::event::WindowEvent::CloseRequested => event_loop.exit(),
+            winit::event::WindowEvent::RedrawRequested => {
+                return self
+                    .graphics_context
+                    .as_ref()
+                    .unwrap()
+                    .window
+                    .as_ref()
+                    .request_redraw();
             }
             _ => (),
         }
@@ -34,20 +78,12 @@ impl ApplicationHandler for App {
 }
 
 fn main() {
-    println!("Hello, world!");
+    pollster::block_on(start());
 }
 
 async fn start() {
-    let instance = Instance::default();
-    let event_loop = EventLoop::new().unwrap();
-
+    let event_loop = winit::event_loop::EventLoop::new().unwrap();
     event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
     let mut app = App::default();
-    event_loop.run_app(&mut app);
-
-    let adapter = instance
-        .request_adapter(&RequestAdapterOptions::default())
-        .await
-        .unwrap();
-    // let surface = instance.create_surface(app.window)
+    event_loop.run_app(&mut app).unwrap();
 }
